d998bd4caf895d0c46b6f0f2a007f6cb2e17b397
Nishant chauhan <nishantchauhannn@gmail.com>
2025-05-16 10:56:54 +0530

    Backend Complete

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './Routes/UserRoutes.js';
 import Chatroutes from './Routes/ChatRoutes.js';
+import cors from cors
  const app = express();
 
  app.use(express.json());
+ app.use(cors())
 
  app.use('/User',router)
  app.use('/Chat',Chatroutes)
